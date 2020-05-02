4cc051f275554e072006d563ffcab45f22814a08
hardervijay <don120994@gmail.com>
2020-05-02 16:03:18 +0530

    First Commit

---
+++ helloworld_demo/src/main/java/helloworld_demo/TestHelloWorld.java
@@ -0,0 +1,14 @@
+package helloworld_demo;
+
+import org.junit.Test;
+
+public class TestHelloWorld {
+	
+	@Test
+	public void  testHelloWorld(){
+		
+		System.out.println("...Hello_World!");
+		
+	}
+
+}
